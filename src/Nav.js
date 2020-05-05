32d9531b572b51ce67084f091f5f21dda8d6d7f7
KellyGauthier <laviolettek@gmail.com>
2020-05-05 14:34:30 +0200

    ajout axios

--- src/Nav.js
+++ src/Nav.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class Nav extends Component {
     render = () => (
         <nav className="navbar navbar-expand-lg navbar-dark bg-secondary sticky-top">
-            <a className="navbar-brand" href="#">Navbar</a>
+            <a className="navbar-brand" href="#">News Broadcaster</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
