bfc63cb080292f66b510713f0fae8a442d064467
KellyGauthier <laviolettek@gmail.com>
2020-05-05 15:20:00 +0200

    liaison back et front

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import './App.scss';
 import Nav from "./Nav";
+import Axios from "axios";
 
 
 const App = () => {
+  let articles = [];
+
+  Axios.get("https://127.0.0.1:8000/api/articles")
+    .then(res => (articles = res.data))
+    .then(() => console.log(articles));
   return (
     <>
       <Nav />
