c3a053ee095e5e58d73687a788883503a1aaa0de
KellyGauthier <laviolettek@gmail.com>
2020-05-05 11:33:00 +0200

    ajout dépendances js de bootstrap

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import "bootstrap";
 
 ReactDOM.render(
   <React.StrictMode>
