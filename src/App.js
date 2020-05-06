e4080f51b824ac151bbf5acce1111b40f7fa1d93
Mouaz Saadaoui <mouazmouaz@hotmail.fr>
2020-05-06 14:46:53 +0200

    style articles cards, convert App.scss to App.css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import "./App.scss";
+import "./App.css";
 import Nav from "./Nav";
 import Axios from "axios";
 
@@ -29,34 +29,46 @@ class App extends Component {
         <Nav />
         <div className="container">
           <div className="row">
-            <div className="col p-2">
+            <div className="col p-2 titles">
               <h1>News Feed</h1>
             </div>
           </div>
           <div className="row">
-            {this.state.articles.map((article) => (
-              <div className="col-12 col-lg-4" key={article.id}>
-                <div className="card" style={{ width: "18rem" }}>
-                  <h5 className="card-title">{article.title}</h5>
-                  <img
-                    className="card-img-top"
-                    src="https://fakeimg.pl/250x100/"
-                    alt="Card image cap"
-                  />
-                  <div className="card-body">
-                    <p className="card-text">{article.description}</p>
-                    <a href="#" className="btn btn-primary">
-                      Go somewhere
-                    </a>
+            <div className="col"></div>
+            <div className="col-6">
+              {this.state.articles.map((article) => (
+                <div
+                  className="row d-flex justify-content-center mb-4"
+                  key={article.id}
+                >
+                  <div className="card w-100">
+                    <div className="card-body">
+                      <h5 className="card-title titles">{article.title}</h5>
+                      <a href={article.url}>
+                        <img
+                          className="card-img-top"
+                          src="https://fakeimg.pl/250x100/"
+                          alt="Card image cap"
+                        />
+                      </a>
+                      <p className="card-text">{article.description}</p>
+                      <a href={article.url} className="btn btn-primary">
+                        Go somewhere
+                      </a>
+                    </div>
                     <div className="card-footer">
                       <small className="text-muted">
                         {moment(article.date).format("LLLL")}
                       </small>
+                      <span class="badge badge-pill badge-primary">
+                        {article.category}
+                      </span>
                     </div>
                   </div>
                 </div>
-              </div>
-            ))}
+              ))}
+            </div>
+            <div className="col"></div>
           </div>
         </div>
       </>
