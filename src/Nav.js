95f54498bcfe09d9156b8f6585b61d7512d6f54f
Mouaz Saadaoui <mouazmouaz@hotmail.fr>
2020-05-11 09:16:12 +0200

    style : change connection btn color in hover

--- src/Nav.js
+++ src/Nav.js
@@ -1,31 +1,57 @@
 import React, { Component } from "react";
 
 class Nav extends Component {
-    render = () => (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-secondary sticky-top">
-            <a className="navbar-brand" href="#">News Broadcaster</a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
+  render = () => (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-secondary sticky-top">
+      <a className="navbar-brand" href="#">
+        News Broadcaster
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
 
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav mr-auto">
-                    <li className="nav-item active">
-                        <a className="nav-link" href="#">Accueil <span className="sr-only">(current)</span></a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#"></a>
-                    </li>
-                </ul>
-                <form className="form-inline my-auto mx-auto w-75">
-                    <input className ="form-control mr-sm-2 w-75"  type="search" placeholder="Tapez votre recherche et peut-être que vous trouverez, on ne sait pas ! Une chance sur 5 d'aprés un des développeurs ..." aria-label="Search" size="90" max-length="30" required></input>
-                    <button className="btn btn-outline-dark my-2 my-sm-0" type="submit">Recherchez!</button>
-                </form>
-                <button type="button" class="btn btn-outline-light">Inscription</button>
-                <button type="button" class="btn btn-light">Connexion</button>
-            </div>
-        </nav>
-    );
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item active">
+            <a className="nav-link" href="#">
+              Accueil <span className="sr-only">(current)</span>
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#"></a>
+          </li>
+        </ul>
+        <form className="form-inline my-auto mx-auto w-75">
+          <input
+            className="form-control mr-sm-2 w-75"
+            type="search"
+            placeholder="Tapez votre mot clé..."
+            aria-label="Search"
+            size="90"
+            max-length="30"
+            required
+          ></input>
+          <button className="btn btn-outline-dark my-2 my-sm-0" type="submit">
+            Rechercher
+          </button>
+        </form>
+        <button type="button" class="btn btn-outline-light">
+          Inscription
+        </button>
+        <button type="button" class="btn btn-light" id="connexion-btn">
+          Connexion
+        </button>
+      </div>
+    </nav>
+  );
 }
 
 export default Nav;
