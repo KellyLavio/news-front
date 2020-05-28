d968bc9c19ed453f4c6a92ebb129d1a2a2743101
KellyGauthier <laviolettek@gmail.com>
2020-05-28 14:19:16 +0200

    add UserPage

---
+++ src/components/Users/UserPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Nav from '../../Nav';
+import MainContainer from '../../MainContainer';
+
+const UserPage = () => (
+    <>
+    <Nav/>
+    <MainContainer/>
+    </>
+);
+
+export default UserPage;
