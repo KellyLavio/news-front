25fdd163bfeed4beaf895558fcc659c01c4a91a4
Mouaz Saadaoui <mouazmouaz@hotmail.fr>
2020-06-04 12:40:50 +0200

    Feat : add user register success message with notification toast and and thumb-up image, Refactor : delete redondant Nav imports in all pages and moves it to App.js before the Router

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,13 +2,11 @@ import React, { useState, useContext } from "react";
 import { useForm } from "react-hook-form";
 import { DoubleBounce } from "better-react-spinkit";
 import history from '../../utils/history';
-
 // import user from "../Users/Users";
 import login from "../../utils/login-utils";
 import LoginErrors from "./LoginErrors";
 import UserContext from "../../context/UserContext";
 import { tokenName } from '../../utils/constants';
-import Nav from "../../Nav";
 
 
 
@@ -44,7 +42,6 @@ const Login = () => {
 
     return (
         <>
-            <Nav />
             <div className="container">
                 <div className="row d-flex justify-content-center">
                     <h1>Connexion</h1>
