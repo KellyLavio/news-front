d468c56753ea5da5f60eac23b7961cefdd9ea6ef
KellyGauthier <laviolettek@gmail.com>
2020-08-28 12:25:18 +0200

    ajout loader2

--- src/components/ProfilPage/EditerProfilPage.js
+++ src/components/ProfilPage/EditerProfilPage.js
@@ -1,22 +1,23 @@
 import React, { useEffect } from "react";
 import { useForm } from "react-hook-form";
-import { userRegister } from "../../utils/register-utils";
 import RegisterErrors from "../Register/RegisterErrors"
 import { useState } from "react";
 import history from "../../utils/history";
 import $ from "jquery";
 import { getPersonalData } from "../../services/profilService";
+import { updateProfil } from "../../utils/edit-profil-utils";
 
 
 const EditerProfilPage = () => {
   const { register, errors, setError, handleSubmit } = useForm();
-  const [loading, setLoading] = useState(false);
+  const [formLoading, setFormLoading] = useState(false);
+  const [pageLoading, setPageLoading] = useState(true);
   const [personnalData, setPersonnalData] = useState([]);
 
   const registering = data => {
-    setLoading(true);
+    setFormLoading(true);
 
-    userRegister(data.name, data.firstname, data.password, data.login, data.email)
+    updateProfil(data.name, data.firstname, data.login, data.email)
       .then(response => {
         if (response.status < 200 || response.status >= 300)
           throw new Error(response);
@@ -24,13 +25,13 @@ const EditerProfilPage = () => {
         return response.json();
       })
       .then(e => {
-        setLoading(false);
-        history.push("/profil/edit");
+        setFormLoading(false);
+        history.push("/profil");
         $(".toast").toast("show");
       })
       .catch(e => {
-        setLoading(false);
-        setError("apiServer", "connection", "Une erreur est survenue");
+        setFormLoading(false);
+        setError("apiServer", "connection", "Une erreur est survenue", "Vos informations ont été modifiées");
       });
   };
 
@@ -38,6 +39,7 @@ const EditerProfilPage = () => {
     getPersonalData()
       .then((res) => {
         setPersonnalData(res.data);
+        setPageLoading(false);
       })
       .catch((err) => console.error(err));
   }, []
@@ -46,108 +48,130 @@ const EditerProfilPage = () => {
 
   return (
     <>
-      <h1 className="d-flex justify-content-center">Modifier mes informations personnelles</h1>
-      <div className="container">
-        <div className="row">
-          <form style={{ width: "100%" }} onSubmit={handleSubmit(registering)}>
-            <div className="row d-flex justify-content-center">
-              <div className="col-md-6">
-                <RegisterErrors errors={errors} />
-              </div>
-            </div>
-            
-            <div className="form-group d-flex justify-content-center">
-              <div className="col-md-6">
-                <label htmlFor="name" className="text-primary">Nom de Famille</label>
-                <input
-                  type="text"
-                  name="name"
-                  className="form-control"
-                  id="name"
-                  ref={register({ required: true })}
-                  aria-describedby="nameHelp"
-                  defaultValue = {personnalData.name}
-                />
-              </div>
-            </div>
-
-            <div className="form-group d-flex justify-content-center">
-              <div className="col-md-6">
-                <label htmlFor="firstname" className="text-primary">Prénom</label>
-                <input
-                  type="text"
-                  name="firstname"
-                  className="form-control"
-                  id="firstname"
-                  ref={register({ required: true })}
-                  aria-describedby="firstNameHelp"
-                  defaultValue = {personnalData.firstname}
-                />
-              </div>
-            </div>
-
-            <div className="form-group d-flex justify-content-center">
-              <div className="col-md-6">
-                <label htmlFor="login" className="text-primary">Login</label>
-                <input
-                  type="text"
-                  name="login"
-                  className="form-control"
-                  id="login"
-                  ref={register({ required: true })}
-                  aria-describedby="emailHelp"
-                  defaultValue = {personnalData.login}
-                />
-              </div>
-            </div>
-
-            <div className="form-group d-flex justify-content-center">
-              <div className="col-md-6">
-                <label htmlFor="email" className="text-primary">E-mail</label>
-                <input
-                  type="email"
-                  name="email"
-                  className="form-control"
-                  id="email"
-                  ref={register({ required: true })}
-                  aria-describedby="emailHelp"
-                  defaultValue = {personnalData.email}
-                />
-              </div>
-            </div>
-            
-            <div className="row d-flex justify-content-center">
-              {loading && (
-                <button className="btn btn-primary" type="button" disabled>
-                  <span
-                    className="spinner-grow spinner-grow-sm"
-                    role="status"
-                    aria-hidden="true"
-                  ></span>
-                  Votre demande est en cours...
-                </button>
-              )}
-              {!loading && (
-                <div>
-                <button
-                  className="btn btn-primary" disabled={loading}>
-                  Mettre à jour
-                </button>
-                <button
-                  type="button"
-                  className="btn btn-danger"
-                  onClick={(e) => {
-                    history.push("/profil");
-                  }}
-                >
-                Retour
-                </button>
-                </div>
-              )}
-            </div>
-          </form>
+      <div className="container-fluid">
+        <div className="row w-100">
+          <div className="col p-2 titles">
+            <h1>Modifier mes informations personnelles</h1>
+          </div>
         </div>
       </div>
+
+      {pageLoading && (
+        <>
+          <div className="d-flex justify-content-center mt-5">
+            <div className="spinner-grow text-primary" role="status">
+              <span className="sr-only"></span>
+            </div>
+          </div>
+          <div className="d-flex justify-content-center text-primary">
+            Chargement de vos données en cours...
+        </div>
+        </>
+      )}
+      {!pageLoading && (
+        <div className="row w-100 d-flex justify-content-center">
+          <div className="col-md-6">
+            <form style={{ width: "100%" }} onSubmit={handleSubmit(registering)}>
+              <div className="row d-flex justify-content-center">
+                <div className="col-md-6">
+                  <RegisterErrors errors={errors} />
+                </div>
+              </div>
+
+              <div className="form-group d-flex justify-content-center">
+                <div className="col-md-6">
+                  <label htmlFor="name" className="text-primary">Nom de Famille</label>
+                  <input
+                    type="text"
+                    name="name"
+                    className="form-control"
+                    id="name"
+                    ref={register({ required: true })}
+                    aria-describedby="nameHelp"
+                    defaultValue={personnalData.name}
+                  />
+                </div>
+              </div>
+
+              <div className="form-group d-flex justify-content-center">
+                <div className="col-md-6">
+                  <label htmlFor="firstname" className="text-primary">Prénom</label>
+                  <input
+                    type="text"
+                    name="firstname"
+                    className="form-control"
+                    id="firstname"
+                    ref={register({ required: true })}
+                    aria-describedby="firstNameHelp"
+                    defaultValue={personnalData.firstname}
+                  />
+                </div>
+              </div>
+
+              <div className="form-group d-flex justify-content-center">
+                <div className="col-md-6">
+                  <label htmlFor="login" className="text-primary">Login</label>
+                  <input
+                    type="text"
+                    name="login"
+                    className="form-control"
+                    id="login"
+                    ref={register({ required: true })}
+                    aria-describedby="emailHelp"
+                    defaultValue={personnalData.login}
+                  />
+                </div>
+              </div>
+
+              <div className="form-group d-flex justify-content-center">
+                <div className="col-md-6">
+                  <label htmlFor="email" className="text-primary">E-mail</label>
+                  <input
+                    type="email"
+                    name="email"
+                    className="form-control"
+                    id="email"
+                    ref={register({ required: true })}
+                    aria-describedby="emailHelp"
+                    defaultValue={personnalData.email}
+                  />
+                </div>
+              </div>
+
+              <div className="row d-flex justify-content-center">
+                {formLoading && (
+                  <button className="btn btn-primary" type="button" disabled>
+                    <span
+                      className="spinner-grow spinner-grow-sm"
+                      role="status"
+                      aria-hidden="true"
+                    ></span>
+                  Votre demande est en cours...
+                  </button>
+                )}
+                {!formLoading && (
+                  <div>
+                    <button
+                      className="btn btn-primary" disabled={formLoading}>
+                      Mettre à jour
+                </button>
+                    <button
+                      type="button"
+                      className="btn btn-danger"
+                      onClick={(e) => {
+                        history.push("/profil");
+                      }}
+                    >
+                      Retour
+                </button>
+                  </div>
+                )}
+              </div>
+            </form>
+          </div>
+        </div>
+      )}
+
     </>
   );
 };
